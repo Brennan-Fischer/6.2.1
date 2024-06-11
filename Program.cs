fe405d43b640c2fc057fbc2c2721bc72d1468a39
Brennan <blfische92@gmail.com>
2024-06-11 15:48:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _6._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stacky stacky = new Stacky(10);
+            Console.WriteLine("Hello, World!");
+            stacky.Push(1);
+            stacky.Push(2);
+            stacky.Push(3);
+            stacky.Print();
+            int var = stacky.Pop();
+            Console.WriteLine($"This var is popped {var}");
+            stacky.Print();
+        }
+    }
+}
---
+++ Stacky.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._2._1
+{
+    internal class Stacky
+    {
+        int Pointer {  get; set; }
+        int[] Value { get; set; }
+        int Size { get; set; }
+        public Stacky(int size) 
+        {
+            Pointer = 0;
+            Size = size;
+            Value = new int[size];
+        }
+        public void Push(int newValue)
+        {
+            if(Pointer==Size)
+            {
+                Console.WriteLine("Stack is full. Must pop prior to adding more values");
+                return;
+            }
+            Value[Pointer++] = newValue;
+            return;
+        }
+        public int Pop()
+        {
+            int var = Value[Pointer - 1];
+            Pointer--;
+            return var;
+        }
+        public void Print()
+        {
+            if(Pointer==0) 
+            {
+                Console.WriteLine("No Values in Array");
+                return;
+            }
+            for(int i = 0; i < Pointer; i++)
+            {
+                Console.WriteLine(Value[i]);
+            }
+            return;
+        }
+
+
+    }
+}
